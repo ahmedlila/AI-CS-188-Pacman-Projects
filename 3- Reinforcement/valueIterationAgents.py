1807e0d8fd7552518de87194b0e398c9908e133e
Ahmed Lila <ahmedelila7@gmail.com>
2022-01-10 10:35:48 +0200

    Q1- ValueIterationAgent

--- 3- Reinforcement/valueIterationAgents.py
+++ 3- Reinforcement/valueIterationAgents.py
@@ -60,8 +60,17 @@ class ValueIterationAgent(ValueEstimationAgent):
         self.runValueIteration()
 
     def runValueIteration(self):
-        # Write value iteration code here
+
         "*** YOUR CODE HERE ***"
+        for i in range(self.iterations):
+            q_current = self.values.copy()
+            for state in self.mdp.getStates():
+                if not self.mdp.isTerminal(state):
+                    policy = self.computeActionFromValues(state)
+                    q_updated = self.computeQValueFromValues(state, policy)
+                    q_current[state] = q_updated
+            self.values = q_current
+
 
 
     def getValue(self, state):
@@ -75,9 +84,23 @@ class ValueIterationAgent(ValueEstimationAgent):
         """
           Compute the Q-value of action in state from the
           value function stored in self.values.
+
+          V(s) = max_{a in actions} Q(s,a)
+          policy(s) = arg_max_{a in actions} Q(s,a)
+
+        #####################################################################
+        # Q(s,a) = sum_{s_ in next states} T(s,a,s_) [R(s,a,s_) + Y V*(s_)] #
+        #####################################################################
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        q = 0
+        q = sum((p * (self.mdp.getReward(state, action, next_state) + self.discount * self.getValue(next_state)))
+                for next_state, p in self.mdp.getTransitionStatesAndProbs(state, action))
+
+        return q
+
+
+
 
     def computeActionFromValues(self, state):
         """
@@ -87,9 +110,16 @@ class ValueIterationAgent(ValueEstimationAgent):
           You may break ties any way you see fit.  Note that if
           there are no legal actions, which is the case at the
           terminal state, you should return None.
+
+        ##############################################
+        # policy(s) = arg_max_{a in actions} Q(s,a)  #
+        ##############################################
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if self.mdp.isTerminal(state): return None
+        policy = max((action for action in self.mdp.getPossibleActions(state)), key = lambda action : self.computeQValueFromValues(state, action))
+        return policy
+
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
